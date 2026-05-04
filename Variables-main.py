48b52c3b0e283e2c367a7123a17d05f3c76c8e19
Elliott-Whiston-tuffnesse <elliott.whiston@stu.ocsb.ca>
2026-05-04 13:55:25 -0400

    Add files via upload

---
+++ Variables-main.py
@@ -0,0 +1,10 @@
+from microbit import *
+# _________ = _________
+# variable  = value (expression)
+
+answer = 42
+hello_message = "hi there!"
+
+display.scroll(hello_message)
+hello_message = "Yo!"
+display.scroll(hello_message)
